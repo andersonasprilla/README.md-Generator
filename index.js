5e1c852918e696a9e75a89ae166cbc78fa0e071d
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-02-13 22:30:58 -0500

    create readme file

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'what is the file name?',
+            name: 'filename'
+        }
+
+    ]).then(response => {
+        fs.writeFile(
+            response.filename + '.md',
+            JSON.stringify(response, null, '\t'),
+            err => err ? console.error(err) :
+                console.log('success'))
+    })
