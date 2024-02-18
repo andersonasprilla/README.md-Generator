7aebc27b0ccdc48e1fb5270cde74d8dca37c9c8e
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-02-17 20:12:52 -0500

    create writeToFile function

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+const markdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+     
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if(err) {
+            return console.log(err)
+        }
+        console.log('Successfully wrote to ' + fileName)
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+
+    })
+}
 
 // Function call to initialize app
 init();
